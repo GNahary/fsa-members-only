cb834a186b84bca18657b7b590ec9edf21d5ff5f
GNahary <gnahary@gmail.com>
2024-08-06 21:13:38 +0300

    Test

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,7 +5,7 @@ export const environment = {
     // The firebase config you retrieved from the console.
     // Note that this is NOT sensitive information
     firebaseConfig: {
-        apiKey: "XXXXXXXXXXXXXXXXXXXX",
+        apiKey: "XXXXXXXXXXXXXXXXXXXXx",
         authDomain: "XXXXXXXXXXXXXXXXXXXX",
         projectId: "XXXXXXXXXXXXXXXXXXXX",
         storageBucket: "XXXXXXXXXXXXXXXXXXXX",
